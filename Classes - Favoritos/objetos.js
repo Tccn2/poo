6c918722d0cc9d19b80a4f324ba55277ca1a20a9
Thayslane <131265884+Tccn2@users.noreply.github.com>
2023-06-06 11:14:38 -0300

    Update objetos.js

--- Classes - Favoritos/objetos.js
+++ Classes - Favoritos/objetos.js
@@ -1,1 +1,265 @@
+let celular1 = new Celular(
+    "Apple",
+    "Iphone 11",
+    "8GB",
+    "60GB",
+    "processador",let celular1 = new Celular(
+    "Apple",
+    "Iphone 11",
+    "8GB",
+    "60GB",
+    "processador",
+    "5",
+    "Apple11.jpeg",
+    "1"
+);
+let celular2 = new Celular(
+    "Samsung",
+    "Algum",
+    "PlaceHolder",
+    "PlaceHolder",
+    "PlaceHolder",
+    "SamsungA23.avif",
+    "2"
+);
+let celular3 = new Celular(
+    "Moto E22",
+    "Algum de cria melhor que da Samsung",
+    "PlaceHolder",
+    "PlaceHolder",
+    "PlaceHolder",
+    "LGK61.jpg",
+    "3"
+);
+let celular4 = new Celular(
+    "Apple",
+    "Iphone 12",
+    "PlaceHolder",
+    "PlaceHolder",
+    "PlaceHolder",
+    "Xiaomi12Pro.jpg",
+    "4"
+);
+let celular5 = new Celular(
+    "LG",
+    "Sei lá",
+    "PlaceHolder",
+    "PlaceHolder",
+    "PlaceHolder",
+    "LGK61.jpg",
+    "5"
+);
 
+
+// document.write(`
+//     <div class='card'>
+//         <img class='foto' src='img/${celular1.foto}' />
+//         <div class='info'>
+//             <h3 class='modelo'>${celular1.modelo}</h3>
+//             <p class='marca'>${celular1.marca}</p>
+            
+//             <span class='outros'>${celular1.processador}</span>
+//             <span class='separador'>|</span>
+//             <span class='outros'>${celular1.armazenamento}</span>
+//             <span class='separador'>|</span>
+//             <span class='outros'>${celular1.ram}</span>
+//             <span class='separador'>|</span>
+//             <span class='outros'>${celular1.qtdDeCameras}</span>
+//             <span class='outros'>câmeras</span>
+//         </div>
+//     </div>
+// `);
+
+let televisao1 = new Tv(
+    "LG", 
+    "Não Sei", 
+    "43", 
+    "LGdeRico.jpg",
+    "6"
+    );
+
+    let televisao2 = new Tv(
+        "Philips", 
+        "Não Sei", 
+        "Não Sei", 
+        "Philips de Cria.jpg",
+        "7"
+        );
+
+    let televisao3 = new Tv(
+            "Roku", 
+            "Não Sei", 
+            "Não Sei", 
+            "RokuTVlixo.png",
+            "8"
+
+            );  
+            
+        let televisao4 = new Tv(
+                "Samsung1", 
+                "Não Sei", 
+                "Não Sei", 
+                "SamsungA23.avif",
+                "9"
+                ); 
+
+        let televisao5 = new Tv(
+                    "Samsung2", 
+                    "Não Sei", 
+                    "Não Sei", 
+                    "SamsungDeRico.PNG",
+                    "10"
+                    ); 
+
+
+    
+
+// Criando 1 lista de Favoritos:
+let meusProdutosTop = new Favoritos();
+//let produtosTopDaDarline = new Favoritos();
+
+// Adicionando produtos À primeira lista de favoritos
+meusProdutosTop.adicionarProduto(celular1);
+meusProdutosTop.adicionarProduto(televisao1);
+
+// Adicionando produtos À segunda lista de favoritos
+//produtosTopDaDarline.adicionarProduto(celular1);
+// produtosTopDaDarline.adicionarProduto(celular5);
+
+// Exibindo os produtos da primeira lista no HTML:
+// meusProdutosTop.exibirNoHTML();
+
+// Exibindo os produtos da segunda lista no HTML:
+// produtosTopDaDarline.exibirNoHTML();
+
+//meusProdutosTop.removerProduto("6")
+//console.log(meusProdutosTop)
+//console.log(produtosTopDaDarline)
+
+
+    "5",
+    "Apple11.jpeg",
+    "1"
+);
+let celular2 = new Celular(
+    "Samsung",
+    "Algum",
+    "PlaceHolder",
+    "PlaceHolder",
+    "PlaceHolder",
+    "SamsungA23.avif",
+    "2"
+);
+let celular3 = new Celular(
+    "Moto E22",
+    "Algum de cria melhor que da Samsung",
+    "PlaceHolder",
+    "PlaceHolder",
+    "PlaceHolder",
+    "LGK61.jpg",
+    "3"
+);
+let celular4 = new Celular(
+    "Apple",
+    "Iphone 12",
+    "PlaceHolder",
+    "PlaceHolder",
+    "PlaceHolder",
+    "Xiaomi12Pro.jpg",
+    "4"
+);
+let celular5 = new Celular(
+    "LG",
+    "Sei lá",
+    "PlaceHolder",
+    "PlaceHolder",
+    "PlaceHolder",
+    "LGK61.jpg",
+    "5"
+);
+
+
+// document.write(`
+//     <div class='card'>
+//         <img class='foto' src='img/${celular1.foto}' />
+//         <div class='info'>
+//             <h3 class='modelo'>${celular1.modelo}</h3>
+//             <p class='marca'>${celular1.marca}</p>
+            
+//             <span class='outros'>${celular1.processador}</span>
+//             <span class='separador'>|</span>
+//             <span class='outros'>${celular1.armazenamento}</span>
+//             <span class='separador'>|</span>
+//             <span class='outros'>${celular1.ram}</span>
+//             <span class='separador'>|</span>
+//             <span class='outros'>${celular1.qtdDeCameras}</span>
+//             <span class='outros'>câmeras</span>
+//         </div>
+//     </div>
+// `);
+
+let televisao1 = new Tv(
+    "LG", 
+    "Não Sei", 
+    "43", 
+    "LGdeRico.jpg",
+    "6"
+    );
+
+    let televisao2 = new Tv(
+        "Philips", 
+        "Não Sei", 
+        "Não Sei", 
+        "Philips de Cria.jpg",
+        "7"
+        );
+
+    let televisao3 = new Tv(
+            "Roku", 
+            "Não Sei", 
+            "Não Sei", 
+            "RokuTVlixo.png",
+            "8"
+
+            );  
+            
+        let televisao4 = new Tv(
+                "Samsung1", 
+                "Não Sei", 
+                "Não Sei", 
+                "SamsungA23.avif",
+                "9"
+                ); 
+
+        let televisao5 = new Tv(
+                    "Samsung2", 
+                    "Não Sei", 
+                    "Não Sei", 
+                    "SamsungDeRico.PNG",
+                    "10"
+                    ); 
+
+
+    
+
+// Criando 1 lista de Favoritos:
+let meusProdutosTop = new Favoritos();
+//let produtosTopDaDarline = new Favoritos();
+
+// Adicionando produtos À primeira lista de favoritos
+meusProdutosTop.adicionarProduto(celular1);
+meusProdutosTop.adicionarProduto(televisao1);
+
+// Adicionando produtos À segunda lista de favoritos
+//produtosTopDaDarline.adicionarProduto(celular1);
+// produtosTopDaDarline.adicionarProduto(celular5);
+
+// Exibindo os produtos da primeira lista no HTML:
+// meusProdutosTop.exibirNoHTML();
+
+// Exibindo os produtos da segunda lista no HTML:
+// produtosTopDaDarline.exibirNoHTML();
+
+//meusProdutosTop.removerProduto("6")
+//console.log(meusProdutosTop)
+//console.log(produtosTopDaDarline)
